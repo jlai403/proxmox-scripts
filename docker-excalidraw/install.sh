0d7aa0e34309e58383378647b62433ce26cd8bfc
jlai403 <joey@jlai.ca>
2025-08-02 00:00:54 -0600

    update install script

--- docker-excalidraw/install.sh
+++ docker-excalidraw/install.sh
@@ -3,8 +3,6 @@
 INSTALL_PATH=/srv/docker/excalidraw/
 
 mkdir -p $INSTALL_PATH
-chmod 750 /srv/docker
-
 wget -P $INSTALL_PATH https://raw.githubusercontent.com/jlai403/proxmox-scripts/refs/heads/main/docker-excalidraw/docker-compose.yml
 
 cd $INSTALL_PATH
