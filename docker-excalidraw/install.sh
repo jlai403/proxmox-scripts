70002dafd8eade30df82315b8164a9270a3993d8
jlai403 <joey@jlai.ca>
2025-08-01 23:55:50 -0600

    fix install

--- docker-excalidraw/install.sh
+++ docker-excalidraw/install.sh
@@ -5,7 +5,7 @@ INSTALL_PATH=/srv/docker/exclidraw/
 mkdir -p $INSTALL_PATH
 chmod 750 /srv/docker
 
-wget -P $INSTALL_PATH https://gitub.com/jlai403/proxmox-scripts/docker-excalidraw/docker-compose.yml
+wget -P $INSTALL_PATH https://raw.githubusercontent.com/jlai403/proxmox-scripts/refs/heads/main/docker-excalidraw/docker-compose.yml
 
 cd $INSTALL_PATH
 docker compose up -d
